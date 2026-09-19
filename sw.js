56da382424349b781754f8111d8ed3b8d5e98761
tylerbridges <tylerbridges1@gmail.com>
2026-09-19 07:11:57 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='rochester-weather-v5';
+const SHELL=['/','/index.html','/brief.html','/manifest.webmanifest','/favicon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))) });
