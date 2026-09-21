9dfea0622728cc77afa2a4034f8f70184883be58
tylerbridges <tylerbridges1@gmail.com>
2026-09-20 19:12:32 -0600

    Simplify radar playback console

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rochester-weather-v44';
+const CACHE='rochester-weather-v45';
 const SHELL=['/','/index.html','/brief.html','/hourly.html','/forecast.html','/radar.html','/credits.html','/settings.html','/manifest.webmanifest','/favicon.svg','/assets/site.css','/assets/weather-core.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
 self.addEventListener('install',()=>self.skipWaiting());
